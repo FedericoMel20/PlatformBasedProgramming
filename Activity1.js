7c83fbe0036c33d3fa219abf8b9ae8654ce0115d
Captain Nacamora <mambuna.bojang_ti23@nusaputra.ac.id>
2024-09-30 17:56:21 +0700

    1st JavaScript Activity

---
+++ Activity1.js
@@ -0,0 +1,17 @@
+//Printing something out
+console.log("Hello JavaScript, I'm Federico");
+//Creating Variables using VAR
+var FirstName = "Federico";
+var LastName = "Melchiorri";
+//Creating a Function
+function Fullname(str1, str2){
+    return str1 + " " + str2
+}
+console.log(Fullname(FirstName, LastName));
+//Creating Variables using "let" and operating on them
+let num1 = 64;
+let num2 = 8;
+console.log(`The Multiplication of ${num1} and ${num2} is : `, num1 * num2);
+console.log(`The addition of ${num1} and ${num2} is : `, num1 + num2);
+console.log(`The Subtraction of ${num1} and ${num2} is : `, num1 - num2);
+console.log(`The Division of ${num1} and ${num2} is : `, num1 / num2);
