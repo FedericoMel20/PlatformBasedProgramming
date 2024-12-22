a91800303d3fac9d01781d26017872300d537299
Captain Nacamora <mambuna.bojang_ti23@nusaputra.ac.id>
2024-12-22 21:38:13 +0700

    Added the UpdateUserById and deleteUserById functions

--- userProfile/models/user-model.js
+++ userProfile/models/user-model.js
@@ -1,8 +1,30 @@
-const db = require('../config/db')
+const db = require('../config/db');
 
-const getAllUsers = async (params) => {
-    const [rows] = await db.query('select * from users')
-    return rows
+const getAllUsers = async () => {
+    const [rows] = await db.query('SELECT * FROM users');
+    return rows;
 }
 
-module.exports = {getAllUsers}
+const getUserById = async (id) => {
+    const [rows] = await db.query('SELECT * FROM users WHERE id=?', id);
+    return rows;
+}
+
+const addUser = async (user) => {
+    const { name, email, phone } = user;
+    const [result] = await db.query('INSERT INTO users (name, email, phone) VALUES (?, ?, ?)', [name, email, phone]);
+    return result.insertId;
+}
+
+const updateUserById = async (id, user) => {
+    const { name, email, phone } = user;
+    const [result] = await db.query('UPDATE users SET name=?, email=?, phone=? WHERE id=?', [name, email, phone, id]);
+    return result.affectedRows;
+}
+
+const deleteUserById = async (id) => {
+    const [result] = await db.query('DELETE FROM users WHERE id=?', id);
+    return result.affectedRows;
+}
+
+module.exports = { getAllUsers , getUserById , addUser, updateUserById, deleteUserById };
--- userProfile/routes/user-route.js
+++ userProfile/routes/user-route.js
@@ -1,9 +1,11 @@
-const userController = require('../controllers/user-controller')
+const userController = require('../controllers/user-controller');
+const express = require('express');
+const router = express.Router();
 
-const express = require('express')
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.post('/', userController.addUser);
+router.put('/:id', userController.updateUserById);
+router.delete('/:id', userController.deleteUserById);
 
-const router = express.Router()
-
-router.get('/',userController.getAllUsers)
-
-module.exports = router
+module.exports = router;
